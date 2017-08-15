59589694f8b5562531c09d1f39501db786e5ab8a
JJH <jjh860627@naver.com>
2017-08-15 17:02:16 +0900

    no message

--- app/src/main/java/com/example/jjh/test/MainActivity.java
+++ app/src/main/java/com/example/jjh/test/MainActivity.java
@@ -1,24 +1,16 @@
 package com.example.jjh.test;
 
-import android.os.Build;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
-import com.example.jjh.test.fragments.Test2Fragment;
 import com.example.jjh.test.fragments.TestFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FrameLayout flContents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,35 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().getAttributes().width = WindowManager.LayoutParams.MATCH_PARENT;
-
         getWindow().getAttributes().height = WindowManager.LayoutParams.MATCH_PARENT;
-
-        flContents = (FrameLayout)findViewById(R.id.flContents);
-
-        findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.flContents, new Test2Fragment());
-                ft.commitAllowingStateLoss();
-            }
-        });
-
-        findViewById(R.id.btnPrev).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.flContents, new TestFragment());
-                ft.commitAllowingStateLoss();
-            }
-        });
-
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.flContents, new TestFragment());
-        ft.commitAllowingStateLoss();
 
         hideSystemUI();
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
@@ -63,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
                 hideSystemUI();
             }
         });
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.flContents, new TestFragment());
+        ft.commitAllowingStateLoss();
+
     }
 
     private void hideSystemUI() {
